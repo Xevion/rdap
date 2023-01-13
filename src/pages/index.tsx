a8373d27cdc379740d00b9401401df37fd330366
Xevion <ryanchwalters@gmail.com>
2023-01-12 21:05:16 -0600

    Commit blank index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+const Index: NextPage = () => {
+    return <main className="flex bg-black items-center justify-center w-screen h-screen">
+        <div className="w-screen text-white text-center">
+            This project is a work-in-progress. Check back soon.
+        </div>
+    </main>
+}
+
+export default Index;
